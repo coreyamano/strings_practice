4111f26d6cc60363deddc8ad93139618fb0328bb
Corey <corey.amano@gmail.com>
2021-06-19 14:01:25 -0700

    solve problem 8

--- strings_practice.rb
+++ strings_practice.rb
@@ -47,11 +47,15 @@
 
 # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
 
-title = "The Fountainhead"
-author = "Ayn Rand"
-p "The author of " + title + " is " + author + "."
+#title = "The Fountainhead"
+#author = "Ayn Rand"
+#p "The author of " + title + " is " + author + "."
 
 # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+title = "The Fountainhead"
+author = "Ayn Rand"
+p "The author of #{title} is #{author}."
 
 # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
 
